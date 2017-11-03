cbe2fa9325484379cdc4965f6fc732588abb44be
benjamin.laschkar <benjamin.laschkar@ekino.com>
2017-11-03 23:28:13 +0100

    Add test lampe

---
+++ test_lampe.py
@@ -0,0 +1,12 @@
+#Test lampe
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+while True:
+    GPIO.output(7, False)
+    time.sleep(0.5)
+    GPIO.output(7, True)
+    time.sleep(0.5)
