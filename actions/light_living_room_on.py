8f74f8e2afa9ac12f43b9b9249f3b294673c5727
benjamin.laschkar <benjamin.laschkar@ekino.com>
2017-11-22 02:26:34 +0100

    modif pin

--- actions/light_living_room_on.py
+++ actions/light_living_room_on.py
@@ -4,7 +4,7 @@ This program turn on living room light.
 The led of the living room is 7 on GPIO.
 """
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO_LAMPE = 7
+GPIO.setmode(GPIO.BCM)
+GPIO_LAMPE = 25
 GPIO.setup(GPIO_LAMPE, GPIO.OUT)
 GPIO.output(GPIO_LAMPE, True)
