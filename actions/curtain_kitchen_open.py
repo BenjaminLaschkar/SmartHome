965696f2aea68f9c5bb736ea4cab557154ca2bfd
benjamin.laschkar <benjamin.laschkar@ekino.com>
2017-11-28 01:38:00 +0100

    Change curtain

--- actions/curtain_kitchen_open.py
+++ actions/curtain_kitchen_open.py
@@ -12,8 +12,8 @@ pwm = PWM(0x40)
 # Note if you'd like more debug output you can instead run:
 # pwm = PWM(0x40, debug=True)
 
-servoMin = 200  # Min pulse length out of 4096
-servoMax = 0  # Max pulse length out of 4096
+servoMin = 0  # Min pulse length out of 4096
+servoMax = 600  # Max pulse length out of 4096
 
 
 def setServoPulse(channel, pulse):
@@ -33,3 +33,4 @@ pwm.setPWM(0, 0, servoMin)
 time.sleep(1)
 pwm.setPWM(0, 0, servoMax)
 time.sleep(1)
+exit()
