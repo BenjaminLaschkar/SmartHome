a01ef35e69c9bc1f1c16e916797c16c07b5793ee
benjamin.laschkar <benjamin.laschkar@ekino.com>
2017-11-28 05:26:09 +0100

    add voice commands

---
+++ voice_action/light_bedroom_on.py
@@ -0,0 +1,21 @@
+"""
+This program manage the light in the bathroom.
+
+The led of the bathroom is 7 on GPIO.
+"""
+import RPi.GPIO as GPIO
+import time
+import sys
+from urllib import request, parse
+sys.path.append('/home/pi/SmartHome/manager/setting')
+from configProxy import getIp, getPort
+
+
+def send(value):
+        data = parse.urlencode({"command": value}).encode()
+        url = "http://" + str(getIp()) + ":" + str(getPort()) + "/sendcommand/"
+        req = request.Request(url, data=data)
+        resp = request.urlopen(req)
+
+
+send("LIGHT_BEDROOM_ON")
